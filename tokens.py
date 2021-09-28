73e4693e8dec1b286227eeb5f89118e10c5ffd15
João Pedro dos Santos <72708214+joaopdss@users.noreply.github.com>
2021-09-27 22:33:50 -0300

    Delete tokens.py

--- tokens.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'rxFLXTshxLnxdyhJ3eA8Mi36I'
-consumer_secret = 'TDk6F46gYJHOn4o0FwY2yrmN3pyYzfj3u5J51hOpGkVtLceRkG'
-access_token = '1417467949510692865-ZQWO72ObjLv8VMoI9uJhNE1mF9Y8Pu'
-access_token_secret = 'XDrm4zV5UwSKxpV6XBpC6TQ2Fd1d9E1bQYJAfVZsdY5T0'
